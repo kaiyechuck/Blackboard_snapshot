ce03ccf7aa8ef0e0b44de112d91f64b3f4a13a9c
kaiyechuck <kaiye@wustl.edu>
2019-10-05 12:11:37 -0500

    1st try

---
+++ test/test10.5.py
@@ -0,0 +1,130 @@
+import os
+import urllib
+import urllib2
+import json
+# import numpy as np
+from scipy import misc
+# import cv2
+import base64
+from PIL import Image
+import time
+
+
+ts = 0
+
+
+def capture(ts):
+    if ts == 0:
+        myCmd = 'fswebcam -r 1280x720 old.jpg'
+        os.system(myCmd)
+        return 1
+
+    elif ts == 1:
+        myCmd = 'fswebcam -r 1280x720 new.jpg'
+        os.system(myCmd)
+        return 2
+
+    else:
+        myCmd = 'cp new.jpg old.jpg'
+        os.system(myCmd)
+
+        myCmd = 'fswebcam -r 1280x720 new.jpg'
+        os.system(myCmd)
+        return 2
+
+    return ts
+
+
+def getAlpha (content):
+    y = json.loads(content)
+    pic = y["scoremap"]
+    # print(len(pic))
+    decoded = base64.decodestring(pic)
+    filename = 'alpha_positive.jpg'
+    with open(filename, 'wb') as handler:
+        handler.write(decoded)
+        print("alpha saved")
+    return filename
+
+
+def configBaiduAip (filename, token):
+    request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_seg"
+    f = open(filename, 'rb')
+    img = base64.b64encode(f.read())
+
+    params = {"image":img, "type":"scoremap"}
+    params = urllib.urlencode(params)
+
+    access_token = token
+    request_url = request_url + "?access_token=" + access_token
+    request = urllib2.Request(url=request_url, data=params)
+    request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+    response = urllib2.urlopen(request)
+    content = response.read()
+
+    return content
+
+
+def gettoken ():
+    host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=UWx8MkEyOCwXjpoYfIAQYj3v&client_secret=jZxUnjOVY9GgIyA9jxjvhFiz1EqoP7Ib'
+    request = urllib2.Request(host)
+    request.add_header('Content-Type', 'application/json; charset=UTF-8')
+    response = urllib2.urlopen(request)
+    content = response.read()
+    if (content):
+        # print("BaiduAip newtoken",content)
+        y = json.loads(content)
+        token = y["access_token"]
+        return token
+    else:
+        print("token error!!!")
+        return
+    # print(token)
+
+
+# Main Function From Here
+while True:
+    ts = capture(ts)
+    # clean the human and merge with the oldone
+    # getAlpha of human
+    if ts == 2:
+        thetoken = gettoken()
+        thecontent = configBaiduAip('new.jpg', thetoken)
+        filename = ''
+        if thecontent:
+            # print thecontent
+            filename = getAlpha(thecontent) #get and save
+
+        alphaImage = misc.imread(filename)
+        alphaImage = 255-alphaImage
+        misc.imsave('dealpha.png', alphaImage)
+
+
+        # merge newone(witout human) with oldone
+        img1 = Image.open('new.jpg')
+        img2 = Image.open('dealpha.png') #filename for alpha
+        img3 = Image.open('old.jpg')
+        mergepic = Image.composite(img1, img3, img2)
+        misc.imsave('temp.png', img1)
+        misc.imsave('new.jpg', mergepic)
+
+        # diff it with the oldone
+        # mergepic = misc.imread('merge.png')
+        oldpic = misc.imread('old.jpg')
+        diff = mergepic - oldpic
+
+        # diff from -255~255, need to compress, mind the way of coding
+        diff_coded = (diff+255)/2
+        misc.imsave('diff_coded.png', diff_coded)
+
+        # need to handle the upload
+        # FIXME
+
+    time.sleep(2)
+
+
+
+
+
+
+
